4d2e1857500fe887a8537f5bf567348365cbd9c6
lunchtmi <lunch@Mac-Pro-de-lunch.local>
2014-10-07 04:08:19 +0200

    agregando jquery y formulario.js

---
+++ js/formulario.js
@@ -0,0 +1,23 @@
+alert("hola bienvenido a la nueva web");
+
+
+
+var $form = $('#formulario'),
+	$titulo = $('#titulo'),
+	$url = $('#url'),
+	$button = $('#mostrar-form'),
+	$list = $('#contenido'),
+	$post = $('.item').first();
+
+
+function mostrarFormulario(){
+	$form.slideToggle();
+	return false;
+}
+
+function agregarPost(){
+	return false;
+}
+// Eventos
+$button.click( mostrarFormulario );
+$form.on('submit', agregarPost);
